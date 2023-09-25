4911a5e1585645eb22e68ddd18fa262eff7e721f
Zach Buth <zachbuth@gmail.com>
2023-09-25 13:36:02 -0400

    NewsRandom

--- Fantasy_Football/ClientApp/src/app/components/news/news.component.ts
+++ Fantasy_Football/ClientApp/src/app/components/news/news.component.ts
@@ -19,7 +19,18 @@ export class NewsComponent implements OnInit {
   ngOnInit(): void {
     // this.subscription = this.timer.subscribe(x=> this.RandomizeNews());
      this.CallApi();
+     setInterval(()=>{
+      this.shuffleArray(this.notlist)
+     },30000)
   }
+
+  shuffleArray(array: any[]): void{
+    for(let i = array.length -1; i>0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array [j]] = [array[j], array[i]];
+    }
+  };
+
 
   CallApi():void{
     
@@ -28,7 +39,7 @@ export class NewsComponent implements OnInit {
          this.notlist = response;
          console.log("call api is working");
          console.log(this.notlist);
-          this.RandomizeNews();
+         this.shuffleArray(this.notlist)
          
        });  
 
