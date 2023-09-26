fff6c53d7fe577ffb33cc83b00f4308aceaf082b
shane <shanechastain1@ymail.com>
2023-09-26 11:46:32 -0400

    reformatted two three pages

--- Fantasy_Football/ClientApp/src/app/components/faq/faq.component.ts
+++ Fantasy_Football/ClientApp/src/app/components/faq/faq.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.css']
 })
 export class FAQComponent implements OnInit {
+  bool1:boolean = false;
+  bool2:boolean = false;
+  bool3:boolean = false;
+  bool4:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
+// toggleDetails(bool:boolean):boolean{
+
+//   bool=!bool;
+//   console.log(bool)
+//   return bool;
+// }
 
 }
