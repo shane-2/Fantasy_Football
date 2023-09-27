2cdfa4840a0f425f30518965697e9621487992d8
Doug Chu <douglas.l.chu@gmail.com>
2023-09-27 15:29:19 -0400

    everything done with navbar except changing hamburger icon to x

--- Fantasy_Football/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ Fantasy_Football/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,61 +1,73 @@
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { Component } from '@angular/core';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.css'],
 })
 export class NavMenuComponent {
-  isExpanded = false;
   user: SocialUser = {} as SocialUser;
   loggedIn: boolean = false;
-  admin:string = "shanechastain10@gmail.com"
-  admin1:string = "zachbuth@gmail.com"
-  admin2:string = "heathj873@gmail.com"
-  admin3:string = "dougy@gmail.com"
-adminp:string = this.user.email;
-yesAdmin:boolean = false;
+  admin: string = 'shanechastain10@gmail.com';
+  admin1: string = 'zachbuth@gmail.com';
+  admin2: string = 'heathj873@gmail.com';
+  admin3: string = 'dougy@gmail.com';
+  adminp: string = this.user.email;
+  yesAdmin: boolean = false;
 
-isAdmin():void{
-  if(this.user.email == this.admin){
-    this.yesAdmin = true;
+  isAdmin(): void {
+    if (this.user.email == this.admin) {
+      this.yesAdmin = true;
+    }
+    if (this.user.email == this.admin1) {
+      this.yesAdmin = true;
+    }
+    if (this.user.email == this.admin2) {
+      this.yesAdmin = true;
+    }
+    if (this.user.email == this.admin3) {
+      this.yesAdmin = true;
+    }
   }
-  if(this.user.email == this.admin1){
-    this.yesAdmin = true;
-  }
-  if(this.user.email == this.admin2){
-    this.yesAdmin = true;
-  }
-  if(this.user.email == this.admin3){
-    this.yesAdmin = true;
-  }
-}
 
-
-
-  constructor(private authService: SocialAuthService) { }
+  constructor(
+    private authService: SocialAuthService,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     //subscribe will activate once logged in
     this.authService.authState.subscribe((user) => {
-        this.user = user;
-        this.loggedIn = (user != null);
-        //consider putting api calls that require user information such as favories in this part right here
-        //console.log(this.user);
-        this.isAdmin();
+      this.user = user;
+      this.loggedIn = user != null;
+      //consider putting api calls that require user information such as favories in this part right here
+      //console.log(this.user);
+      this.isAdmin();
     });
   }
 
   signOut(): void {
     this.authService.signOut();
-    }
-
-  collapse() {
-    this.isExpanded = false;
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
+  toggleMenu() {
+    const navigation = document.querySelector('.navigation');
+    const navToggle = document.querySelector('.mobile-nav-toggle');
+
+    if (navigation && navToggle) {
+      navToggle.addEventListener('click', () => {
+        const visibility = navigation.getAttribute('data-visible');
+
+        if (visibility === 'false') {
+          navigation.setAttribute('data-visible', 'true');
+          navToggle.setAttribute('aria-expanded', 'true');
+        } else if (visibility === 'true') {
+          navigation.setAttribute('data-visible', 'false');
+          navToggle.setAttribute('aria-expanded', 'false');
+        }
+      });
+    }
   }
 }
