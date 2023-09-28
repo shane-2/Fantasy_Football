31d220c4c330075375606d9a6a647f00163356e7
Doug Chu <douglas.l.chu@gmail.com>
2023-09-27 23:02:52 -0400

    styling on add+drop component

--- Fantasy_Football/ClientApp/src/app/components/add-drop/add-drop.component.ts
+++ Fantasy_Football/ClientApp/src/app/components/add-drop/add-drop.component.ts
@@ -34,4 +34,14 @@ getTeamColor(team: string): string {
   return `var(--team-color-${team})`
 }
 
+getGradientBackground(color: string): string {
+  const gradientStops = [];
+  for (let i = 0; i < this.list.most_added.length; i++) {
+    const alpha = 1 - (i / (this.list.most_added.length - 1)); // Calculate alpha based on index
+    const colorWithAlpha = `rgba(255, 255, 255, ${alpha})`; // Color with dynamic alpha
+    gradientStops.push(`${colorWithAlpha} ${(i + 1) * 10}%`);
+  }
+  return `linear-gradient(to bottom, ${gradientStops.join(',')})`;
+}
+
 }
