02a0c15051b43c7d008007ecad431de29ede37fd
Doug Chu <douglas.l.chu@gmail.com>
2023-10-03 14:22:13 -0400

    styled watchlist to match ranking page

--- Fantasy_Football/ClientApp/src/app/components/admin/admin.component.ts
+++ Fantasy_Football/ClientApp/src/app/components/admin/admin.component.ts
@@ -5,64 +5,63 @@ import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
 })
 export class AdminComponent implements OnInit {
+  constructor(
+    private _fantasyService: FantasyService,
+    private authService: SocialAuthService
+  ) {}
+  allPlayers: FantasyFolk[] = [];
 
-  constructor(private _fantasyService:FantasyService, private authService: SocialAuthService) { }
-  allPlayers:FantasyFolk[] =[];
-
- 
   ngOnInit(): void {
-    this.yesAdmin= false;
+    this.yesAdmin = false;
     this.authService.authState.subscribe((user) => {
       this.user = user;
-      this.loggedIn = (user != null);
+      this.loggedIn = user != null;
       this.isAdmin();
     });
   }
-  UseVoterFraud():void{
-   
-       this._fantasyService.GetVoterFraud().subscribe((response:FantasyFolk[])=>{      
+  UseVoterFraud(): void {
+    this._fantasyService
+      .GetVoterFraud()
+      .subscribe((response: FantasyFolk[]) => {
         this.allPlayers = response;
         console.log(this.allPlayers);
-  });
+      });
+  }
 
-}
+  FolkListResult: FantasyFolk[] = [];
 
-FolkListResult: FantasyFolk [] = [];
+  NewFolk(newFolk: FantasyFolk) {
+    this._fantasyService.AddFolk(newFolk).subscribe((response: FantasyFolk) => {
+      console.log(response);
+      this.FolkListResult.push(response);
+    });
+  }
 
-NewFolk(newFolk:FantasyFolk){
-  this._fantasyService.AddFolk(newFolk).subscribe((response: FantasyFolk) =>{
-   console.log(response);
-   this.FolkListResult.push(response);
-  })
- }
+  user: SocialUser = {} as SocialUser;
+  loggedIn: boolean = false;
 
- user: SocialUser = {} as SocialUser;
- loggedIn: boolean = false;
+  admin: string = 'shanechastain10@gmail.com';
+  admin1: string = 'zachbuth@gmail.com';
+  admin2: string = 'heathj873@gmail.com';
+  admin3: string = 'doug.e.chu@gmail.com';
+  adminp: string = this.user.email;
+  yesAdmin: boolean = false;
 
- admin:string = "shanechastain10@gmail.com"
- admin1:string = "zachbuth@gmail.com"
- admin2:string = "heathj873@gmail.com"
- admin3:string = "dougy@gmail.com"
-adminp:string = this.user.email;
-yesAdmin:boolean = false;
-
-isAdmin():void{
- if(this.user.email == this.admin){
-   this.yesAdmin = true;
- }
- if(this.user.email == this.admin1){
-   this.yesAdmin = true;
- }
- if(this.user.email == this.admin2){
-   this.yesAdmin = true;
- }
- if(this.user.email == this.admin3){
-   this.yesAdmin = true;
- }
-
-
-}
+  isAdmin(): void {
+    if (this.user.email == this.admin) {
+      this.yesAdmin = true;
+    }
+    if (this.user.email == this.admin1) {
+      this.yesAdmin = true;
+    }
+    if (this.user.email == this.admin2) {
+      this.yesAdmin = true;
+    }
+    if (this.user.email == this.admin3) {
+      this.yesAdmin = true;
+    }
+  }
 }
--- Fantasy_Football/ClientApp/src/app/components/watchlist/watchlist.component.ts
+++ Fantasy_Football/ClientApp/src/app/components/watchlist/watchlist.component.ts
@@ -5,66 +5,84 @@ import { FantasyService } from 'src/app/services/fantasy.service';
 import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { FantasyFolk } from 'src/app/models/fantasy-folk';
 
-
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
-  styleUrls: ['./watchlist.component.css']
+  styleUrls: ['./watchlist.component.css'],
 })
 export class WatchlistComponent implements OnInit {
-  constructor(private _fantasyService:FantasyService, private router:Router, private authService: SocialAuthService) { }
-  
-  watchlist:Watchlist[] = [];
-  fantasyList:FantasyFolk[] = [];
+  watchButtonHovered: boolean[] = [];
+  deleteButtonHovered: boolean[] = [];
 
-  loggedIn: boolean = false;
+  constructor(
+    private _fantasyService: FantasyService,
+    private router: Router,
+    private authService: SocialAuthService
+  ) {}
+
+  watchlist: Watchlist[] = [];
+  fantasyList: FantasyFolk[] = [];
+
+  loggedIn: boolean = true;
   user: SocialUser = {} as SocialUser;
 
-  
   ngOnInit(): void {
-  this.authService.authState.subscribe((user) => {
-    this.user = user;
-    this.loggedIn = (user != null);
-	});
-   this.DisplayWatchList(this.user);
-  }  
-  
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = user != null;
+    });
+    this.DisplayWatchList(this.user);
 
-  DisplayWatchList(user:SocialUser):void{
-  this._fantasyService.GetWatchlist(user.email).subscribe((response:Watchlist[]) =>{
-    console.log("display watchlist");
-    console.log(response);
-   this.watchlist = response;
-  this.DisplayFantasyFolk();
-  });
-}
+    //button properties
+    this.watchButtonHovered = Array(this.watchlist.length).fill(false);
+    this.deleteButtonHovered = Array(this.watchlist.length).fill(false);
+  }
 
-    DeletePlayer(id:number):void{
-      // let id = parseInt(myString);
-      //feedback for user
-      let target:number = this.watchlist.findIndex(e => e.id == id);
-      this.watchlist.splice(target,1);
-  
-      this._fantasyService.DeleteWatchlistPlayer(id, this.user.email).subscribe((response:Watchlist) => {
+  DisplayWatchList(user: SocialUser): void {
+    this._fantasyService
+      .GetWatchlist(user.email)
+      .subscribe((response: Watchlist[]) => {
+        console.log('display watchlist');
+        console.log(response);
+        this.watchlist = response;
+        this.DisplayFantasyFolk();
+      });
+  }
+
+  DeletePlayer(id: number): void {
+    // let id = parseInt(myString);
+    //feedback for user
+    let target: number = this.watchlist.findIndex((e) => e.id == id);
+    this.watchlist.splice(target, 1);
+
+    this._fantasyService
+      .DeleteWatchlistPlayer(id, this.user.email)
+      .subscribe((response: Watchlist) => {
         console.log(response);
         this.DisplayWatchList(this.user);
       });
-      
   }
-
-
 
   DisplayFantasyFolk(): void {
     this.fantasyList = [];
-    this.watchlist.forEach(p=>{
-      this._fantasyService.GetFolksInWatchlist(p.playerId).subscribe((response:FantasyFolk) => {
-        console.log(response);
-        this.fantasyList.push(response);
-      });
-
-    })
+    this.watchlist.forEach((p) => {
+      this._fantasyService
+        .GetFolksInWatchlist(p.playerId)
+        .subscribe((response: FantasyFolk) => {
+          console.log(response);
+          this.fantasyList.push(response);
+        });
+    });
   }
   getTeamColor(team: string): string {
-    return `var(--team-color-${team})`
+    return `var(--team-color-${team})`;
+  }
+
+  onDeleteButtonMouseEnter(index: number) {
+    this.deleteButtonHovered[index] = true;
+  }
+
+  onDeleteButtonMouseLeave(index: number) {
+    this.deleteButtonHovered[index] = false;
   }
 }
