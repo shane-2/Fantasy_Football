dbd5d0772417125e6c41cd00e8207ba629c04d06
Zach Buth <zachbuth@gmail.com>
2023-11-27 21:00:01 -0500

    5wk

--- Fantasy_Football/ClientApp/src/app/components/player-detail/player-detail.component.ts
+++ Fantasy_Football/ClientApp/src/app/components/player-detail/player-detail.component.ts
@@ -66,7 +66,7 @@ export class PlayerDetailComponent implements OnInit {
 
   setplayer: PlayerElement = this._fantasyService.getsetPlayer();
 
-  divider: number = 8.5;
+  divider: number = 5;
 
   player: Qb | Rb | K | Def | Idp | undefined;
 
